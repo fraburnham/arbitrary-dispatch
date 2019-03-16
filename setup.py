3b7689df288e10ede01257c1fc344b1d807e7416
Frank Burnham <frank.e.burnham@gmail.com>
2019-03-16 16:46:05 -0400

    Add setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='arbitrary_dispatch',
+    version='0.0.1',
+    description='Dispatch via an arbitrary function',
+    url='https://www.github.com/fraburnham/arbitrary-dispatch'
+)
